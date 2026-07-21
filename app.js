8172eb65d60005ea88c81d99c3a6c59c745cda22
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-07-21 14:39:54 +0300

    update

---
+++ app.js
@@ -0,0 +1,70 @@
+console.log('Works!');
+let favSnack;
+// let is the declaration
+// favSnack is the identifier
+
+let name = 'Tony Stark';
+
+name = 'Pepper Potts';
+// note that we only declare a variable once
+
+/*
+let x = 25;
+x = 100; // no problem
+const z = 25;
+z = 100; // Uncaught TypeError: Assignment to constant variable
+*/
+
+let myString = 'Hello World';
+let myOtherString = "Hello World again";
+console.log(typeof myString);
+// Prints: strings
+
+
+let myNumber = 15;
+console.log(typeof myNumber);
+// Prints: number
+
+console.log(typeof 12.34);
+
+
+
+let cohort;
+console.log(cohort);
+// Prints: undefined
+
+
+console.log(typeof null);
+// Prints: object
+
+
+let myName = 'Hasan Mohammed';
+let friendName = 'S.youseif';
+
+console.log("My name is " + myName);
+console.log("My friend's name is " + friendName);
+
+
+let total = typeof "4" + 1;
+console.log(total);
+
+let make = typeof 1 + " is a number";
+console.log(make);
+
+
+let trueValue = "1" == 1;
+console.log(trueValue);
+
+
+let falseValue = "1" === 1;
+console.log(falseValue);
+
+
+
+
+
+
+/*
+greet + userName;
+
+*/
